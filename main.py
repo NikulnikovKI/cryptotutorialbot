b807533c0059f84090722c0b6a243dd5828bf383
NikulnikovKi <Nikulnikovki@rambler.ru>
2024-09-03 20:47:55 +0400

    pro2-masre: init project

---
+++ main.py
@@ -0,0 +1,33 @@
+import telebot
+
+
+TOKEN = '7163965371:AAFrtz_SFSdVRjCeTSFwtFYrqMqwnc6qbto'
+
+
+bot = telebot.TeleBot(TOKEN)
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+}
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите комманду боту в следующем формате:\n<имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты>\nУвидить список всех доступных волют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
+    bot.reply_to(message, text)
+
+
+
+bot.polling()
